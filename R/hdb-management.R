84e2a6e35abc559db1962f4d51804b15290b5c0e
david-hammond <dhammond@economicsandpeace.org>
2019-06-13 17:34:38 +1000

    master key not working

--- R/hdb-management.R
+++ R/hdb-management.R
@@ -55,7 +55,6 @@ hdb_update_master = function(){
   WHERE datistemplate = false;")
   dbDisconnect(con)
   dbs = dbs %>% filter(!(datname %in% c("postgres", "master")))
-  master_key = NULL
   db_index = function(db){
     con <- hdb_connect(db)
     key = dbReadTable(con, "key")
@@ -68,16 +67,18 @@ hdb_update_master = function(){
       tmp = suppressMessages(tmp %>% left_join(key %>% select(uid, seriescode)) %>% select(uid, geocode, year, value))
       dbWriteTable(con, tab, tmp, overwrite = T)
     }
-    master_key = rbind(master_key, key)
+    dbWriteTable(con, "key", key, overwrite = T, row.names = F)
     dbDisconnect(con)
+    return(key)
   }
-  pblapply(dbs$datname, db_index)
+  master_key = pblapply(dbs$datname, db_index)
+  master_key = bind_rows(master_key)
   db = "master"
   con = hdb_create_db(db)
   tab = "key"
   dbWriteTable(con, tab, master_key, overwrite = T, row.names = F)
-  query = paste0('ALTER TABLE ', tab, ' ADD CONSTRAINT id_', tab,
-                 '_pk PRIMARY KEY ("uid");')
+  #query = paste0('ALTER TABLE ', tab, ' ADD CONSTRAINT id_', tab,
+  #               '_pk PRIMARY KEY ("uid");')
   dbGetQuery(con, query)
   dbDisconnect(con)
 }
