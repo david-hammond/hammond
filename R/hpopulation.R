ea0c21a0f1182b89f45fd06aeb9674e41e419c52
david-hammond <dhammond@economicsandpeace.org>
2019-08-02 16:20:13 +1000

    population fix

--- R/hpopulation.R
+++ R/hpopulation.R
@@ -11,11 +11,14 @@
 hpopulation = function(df){
     require(wbstats)
     require(dplyr)
+    df$year = lubridate::year(df$date)
     pop_data <- wb(indicator = "SP.POP.TOTL", startdate = min(df$year), end = max(df$year))
     pop_data = pop_data %>%
       select(iso3c, date, value) %>%
       rename(geocode = iso3c, year = date, population = value) %>%
       mutate(year = as.numeric(year))
+
     df = dplyr::left_join(df, pop_data)
+    df = df %>% select(-year)
     return(df)
 }
