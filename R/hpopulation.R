2eabcd46c1da7043aa4be2c9a84f0a6b24b9890e
david-hammond <dhammond@economicsandpeace.org>
2019-08-02 14:54:53 +1000

    hpopulation

---
+++ R/hpopulation.R
@@ -0,0 +1,21 @@
+#' hpopulation
+#'
+#' This function appends a column of populations to a dataframe
+#'
+#' @param df
+#'
+#' @examples
+#' x = hpopulation(hammond::hcountryexampledata)
+#'
+#' @export
+hpopulation = function(df){
+    require(wbstats)
+    require(dplyr)
+    pop_data <- wb(indicator = "SP.POP.TOTL", startdate = min(df$year), end = max(df$year))
+    pop_data = pop_data %>%
+      select(iso3c, date, value) %>%
+      rename(geocode = iso3c, year = date, population = value) %>%
+      mutate(year = as.numeric(year))
+    df = dplyr::left_join(df, pop_data)
+    return(df)
+}
